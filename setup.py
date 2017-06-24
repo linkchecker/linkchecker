9320d595f26913ce1436d686540be51a9c830612
Eugene Yaremenko <w3techplayground@gmail.com>
2017-06-24 21:30:55 +0300

    Temporary fix
    Linkchecker doesn't work with the latest requests

--- setup.py
+++ setup.py
@@ -480,7 +480,7 @@ args = dict(
     },
     # Requirements, usable with setuptools or the new Python packaging module.
     install_requires = [
-        'requests >= 2.2.0',
+        'requests<2.15,>=2.2',
     ],
     # Commented out since they are untested and not officially supported.
     # See also doc/install.txt for more detailed dependency documentation.
