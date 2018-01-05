4be393a0bc91c2821136fbd9a27c9a23ee86ea62
Petr Dlouhý <petr.dlouhy@email.cz>
2018-01-05 20:31:32 +0100

    Python3: fix url.enswith in url.py

--- linkcheck/url.py
+++ linkcheck/url.py
@@ -357,7 +357,7 @@ def url_norm (url, encoding=None):
         urlparts[2] = url_fix_wayback_query(urlparts[2]) # unencode colon in http[s]:// in wayback path
     urlparts[4] = url_quote_part(urlparts[4], safechars="!$&'()*+,-./;=?@_~", encoding=encoding) # anchor
     res = urlunsplit(urlparts)
-    if url.endswith(b'#') and not urlparts[4]:
+    if decode_for_unquote(url).endswith('#') and not urlparts[4]:
         # re-append trailing empty fragment
         res += '#'
     if encode_unicode:
