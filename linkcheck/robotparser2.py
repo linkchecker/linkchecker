PetrDlouhy/linkchecker-1

Python3: fix imports

linkcheck/robotparser2.py
@@ -20,12 +20,14 @@ Robots.txt parser.
 The robots.txt Exclusion Protocol is implemented as specified in
 http://www.robotstxt.org/wc/norobots-rfc.html
 """
-try:
-    import urlparse
-except ImportError:
-    # Python 3
+try: # Python 3
+    from urllib import parse as urllib_parse
+except ImportError: # Python 2
+    import urllib as urllib_parse
+try: # Python 3
     from urllib import parse as urlparse
-import urllib
+except ImportError: # Python 2
+    import urlparse
 import time
 import requests
 from . import log, LOG_CHECK, configuration
@@ -162,7 +164,7 @@ class RobotFileParser (object):
             line = line.split(':', 1)
             if len(line) == 2:
                 line[0] = line[0].strip().lower()
-                line[1] = urllib.unquote(line[1].strip())
+                line[1] = urllib_parse.unquote(line[1].strip())
                 if line[0] == "user-agent":
                     if state == 2:
                         log.debug(LOG_CHECK, "%r line %d: missing blank line before user-agent directive", self.url, linenumber)
@@ -230,7 +232,7 @@ class RobotFileParser (object):
             return True
         # search for given user agent matches
         # the first match counts
-        url = urllib.quote(urlparse.urlparse(urllib.unquote(url))[2]) or "/"
+        url = urllib_parse.quote(urlparse.urlparse(urllib_parse.unquote(url))[2]) or "/"
         for entry in self.entries:
             if entry.applies_to(useragent):
                 return entry.allowance(url)
@@ -276,7 +278,7 @@ class RuleLine (object):
             # an empty value means allow all
             allowance = True
             path = '/'
-        self.path = urllib.quote(path)
+        self.path = urllib_parse.quote(path)
         self.allowance = allowance
 
     def applies_to (self, path):

linkcheck/url.py
@@ -20,13 +20,14 @@ Functions for parsing and matching URL strings.
 
 import re
 import os
-try:
-    import urlparse
-except ImportError:
-    # Python 3
+try: # Python 3
+    from urllib import parse as urllib_parse
     from urllib import parse as urlparse
-import urllib
+except ImportError: # Python 2
+    import urllib as urllib_parse
+    import urlparse
 import requests
+
 from . import log, LOG_CHECK
 
 for scheme in ('ldap', 'irc'):
@@ -162,9 +163,9 @@ def parse_qsl (qs, keep_blank_values=0, strict_parsing=0):
             else:
                 continue
         if nv[1] or keep_blank_values:
-            name = urllib.unquote(nv[0].replace('+', ' '))
+            name = urllib_parse.unquote(nv[0].replace('+', ' '))
             if nv[1]:
-                value = urllib.unquote(nv[1].replace('+', ' '))
+                value = urllib_parse.unquote(nv[1].replace('+', ' '))
             else:
                 value = nv[1]
             r.append((name, value, sep))
@@ -189,12 +190,12 @@ def idna_encode (host):
 def url_fix_host (urlparts):
     """Unquote and fix hostname. Returns is_idn."""
     if not urlparts[1]:
-        urlparts[2] = urllib.unquote(urlparts[2])
+        urlparts[2] = urllib_parse.unquote(urlparts[2])
         return False
-    userpass, netloc = urllib.splituser(urlparts[1])
+    userpass, netloc = urllib_parse.splituser(urlparts[1])
     if userpass:
-        userpass = urllib.unquote(userpass)
-    netloc, is_idn = idna_encode(urllib.unquote(netloc).lower())
+        userpass = urllib_parse.unquote(userpass)
+    netloc, is_idn = idna_encode(urllib_parse.unquote(netloc).lower())
     # a leading backslash in path causes urlsplit() to add the
     # path components up to the first slash to host
     # try to find this case...
@@ -205,7 +206,7 @@ def url_fix_host (urlparts):
         if not urlparts[2] or urlparts[2] == '/':
             urlparts[2] = comps
         else:
-            urlparts[2] = "%s%s" % (comps, urllib.unquote(urlparts[2]))
+            urlparts[2] = "%s%s" % (comps, urllib_parse.unquote(urlparts[2]))
         netloc = netloc[:i]
     else:
         # a leading ? in path causes urlsplit() to add the query to the
@@ -214,7 +215,7 @@ def url_fix_host (urlparts):
         if i != -1:
             netloc, urlparts[3] = netloc.split('?', 1)
         # path
-        urlparts[2] = urllib.unquote(urlparts[2])
+        urlparts[2] = urllib_parse.unquote(urlparts[2])
     if userpass:
         # append AT for easy concatenation
         userpass += "@"
@@ -311,7 +312,7 @@ def url_norm (url, encoding=None):
         encode_unicode = False
     urlparts = list(urlparse.urlsplit(url))
     # scheme
-    urlparts[0] = urllib.unquote(urlparts[0]).lower()
+    urlparts[0] = urllib_parse.unquote(urlparts[0]).lower()
     # mailto: urlsplit is broken
     if urlparts[0] == 'mailto':
         url_fix_mailto_urlsplit(urlparts)
@@ -331,7 +332,7 @@ def url_norm (url, encoding=None):
             # fix redundant path parts
             urlparts[2] = collapse_segments(urlparts[2])
     # anchor
-    urlparts[4] = urllib.unquote(urlparts[4])
+    urlparts[4] = urllib_parse.unquote(urlparts[4])
     # quote parts again
     urlparts[0] = url_quote_part(urlparts[0], encoding=encoding) # scheme
     urlparts[1] = url_quote_part(urlparts[1], safechars='@:', encoding=encoding) # host
@@ -418,11 +419,11 @@ def url_quote_part (s, safechars='/', encoding=None):
         if encoding is None:
             encoding = url_encoding
         s = s.encode(encoding, 'ignore')
-    return urllib.quote(s, safechars)
+    return urllib_parse.quote(s, safechars)
 
 def document_quote (document):
     """Quote given document."""
-    doc, query = urllib.splitquery(document)
+    doc, query = urllib_parse.splitquery(document)
     doc = url_quote_part(doc, '/=,')
     if query:
         return "%s?%s" % (doc, query)
@@ -473,8 +474,8 @@ def url_split (url):
     hostname is always lowercased.
     Precondition: url is syntactically correct URI (eg has no whitespace)
     """
-    scheme, netloc = urllib.splittype(url)
-    host, document = urllib.splithost(netloc)
+    scheme, netloc = urllib_parse.splittype(url)
+    host, document = urllib_parse.splithost(netloc)
     port = default_ports.get(scheme, 0)
     if host:
         host = host.lower()

linkchecker/linkchecker

linkcheck/robotparser2.py
@@ -20,19 +20,24 @@ Robots.txt parser.
 The robots.txt Exclusion Protocol is implemented as specified in
 http://www.robotstxt.org/wc/norobots-rfc.html
 """
-try:
-    import urlparse
-except ImportError:
-    # Python 3
-    from urllib import parse as urlparse
-import urllib
+try:  # Python 3
+    from urllib import parse
+except ImportError:  # Python 2
+    import urllib as parse
+try:  # Python 3
+    from urllib.parse import urlparse
+except ImportError:  # Python 2
+    from urlparse import urlparse
 import time
+
 import requests
+
 from . import log, LOG_CHECK, configuration
 
 __all__ = ["RobotFileParser"]
 
 ACCEPT_ENCODING = 'x-gzip,gzip,deflate'
+
 
 class RobotFileParser (object):
     """This class provides a set of methods to read, parse and answer
@@ -79,7 +84,7 @@ class RobotFileParser (object):
     def set_url (self, url):
         """Set the URL referring to a robots.txt file."""
         self.url = url
-        self.host, self.path = urlparse.urlparse(url)[1:3]
+        self.host, self.path = urlparse(url)[1:3]
 
     def read (self):
         """Read the robots.txt URL and feeds it to the parser."""
@@ -162,7 +167,7 @@ class RobotFileParser (object):
             line = line.split(':', 1)
             if len(line) == 2:
                 line[0] = line[0].strip().lower()
-                line[1] = urllib.unquote(line[1].strip())
+                line[1] = parse.unquote(line[1].strip())
                 if line[0] == "user-agent":
                     if state == 2:
                         log.debug(LOG_CHECK, "%r line %d: missing blank line before user-agent directive", self.url, linenumber)
@@ -230,7 +235,7 @@ class RobotFileParser (object):
             return True
         # search for given user agent matches
         # the first match counts
-        url = urllib.quote(urlparse.urlparse(urllib.unquote(url))[2]) or "/"
+        url = parse.quote(urlparse(parse.unquote(url))[2]) or "/"
         for entry in self.entries:
             if entry.applies_to(useragent):
                 return entry.allowance(url)
@@ -276,7 +281,7 @@ class RuleLine (object):
             # an empty value means allow all
             allowance = True
             path = '/'
-        self.path = urllib.quote(path)
+        self.path = parse.quote(path)
         self.allowance = allowance
 
     def applies_to (self, path):

linkcheck/url.py
@@ -18,15 +18,17 @@
 Functions for parsing and matching URL strings.
 """
 
-import re
 import os
-try:
-    import urlparse
-except ImportError:
-    # Python 3
+import re
+try:  # Python 3
+    from urllib import parse
     from urllib import parse as urlparse
-import urllib
+except ImportError:  # Python 2
+    import urllib as parse
+    import urlparse
+
 import requests
+
 from . import log, LOG_CHECK
 
 for scheme in ('ldap', 'irc'):
@@ -162,9 +164,9 @@ def parse_qsl (qs, keep_blank_values=0, strict_parsing=0):
             else:
                 continue
         if nv[1] or keep_blank_values:
-            name = urllib.unquote(nv[0].replace('+', ' '))
+            name = parse.unquote(nv[0].replace('+', ' '))
             if nv[1]:
-                value = urllib.unquote(nv[1].replace('+', ' '))
+                value = parse.unquote(nv[1].replace('+', ' '))
             else:
                 value = nv[1]
             r.append((name, value, sep))
@@ -189,12 +191,12 @@ def idna_encode (host):
 def url_fix_host (urlparts):
     """Unquote and fix hostname. Returns is_idn."""
     if not urlparts[1]:
-        urlparts[2] = urllib.unquote(urlparts[2])
+        urlparts[2] = parse.unquote(urlparts[2])
         return False
-    userpass, netloc = urllib.splituser(urlparts[1])
+    userpass, netloc = parse.splituser(urlparts[1])
     if userpass:
-        userpass = urllib.unquote(userpass)
-    netloc, is_idn = idna_encode(urllib.unquote(netloc).lower())
+        userpass = parse.unquote(userpass)
+    netloc, is_idn = idna_encode(parse.unquote(netloc).lower())
     # a leading backslash in path causes urlsplit() to add the
     # path components up to the first slash to host
     # try to find this case...
@@ -205,7 +207,7 @@ def url_fix_host (urlparts):
         if not urlparts[2] or urlparts[2] == '/':
             urlparts[2] = comps
         else:
-            urlparts[2] = "%s%s" % (comps, urllib.unquote(urlparts[2]))
+            urlparts[2] = "%s%s" % (comps, parse.unquote(urlparts[2]))
         netloc = netloc[:i]
     else:
         # a leading ? in path causes urlsplit() to add the query to the
@@ -214,7 +216,7 @@ def url_fix_host (urlparts):
         if i != -1:
             netloc, urlparts[3] = netloc.split('?', 1)
         # path
-        urlparts[2] = urllib.unquote(urlparts[2])
+        urlparts[2] = parse.unquote(urlparts[2])
     if userpass:
         # append AT for easy concatenation
         userpass += "@"
@@ -311,7 +313,7 @@ def url_norm (url, encoding=None):
         encode_unicode = False
     urlparts = list(urlparse.urlsplit(url))
     # scheme
-    urlparts[0] = urllib.unquote(urlparts[0]).lower()
+    urlparts[0] = parse.unquote(urlparts[0]).lower()
     # mailto: urlsplit is broken
     if urlparts[0] == 'mailto':
         url_fix_mailto_urlsplit(urlparts)
@@ -331,7 +333,7 @@ def url_norm (url, encoding=None):
             # fix redundant path parts
             urlparts[2] = collapse_segments(urlparts[2])
     # anchor
-    urlparts[4] = urllib.unquote(urlparts[4])
+    urlparts[4] = parse.unquote(urlparts[4])
     # quote parts again
     urlparts[0] = url_quote_part(urlparts[0], encoding=encoding) # scheme
     urlparts[1] = url_quote_part(urlparts[1], safechars='@:', encoding=encoding) # host
@@ -418,11 +420,11 @@ def url_quote_part (s, safechars='/', encoding=None):
         if encoding is None:
             encoding = url_encoding
         s = s.encode(encoding, 'ignore')
-    return urllib.quote(s, safechars)
+    return parse.quote(s, safechars)
 
 def document_quote (document):
     """Quote given document."""
-    doc, query = urllib.splitquery(document)
+    doc, query = parse.splitquery(document)
     doc = url_quote_part(doc, '/=,')
     if query:
         return "%s?%s" % (doc, query)
@@ -473,8 +475,8 @@ def url_split (url):
     hostname is always lowercased.
     Precondition: url is syntactically correct URI (eg has no whitespace)
     """
-    scheme, netloc = urllib.splittype(url)
-    host, document = urllib.splithost(netloc)
+    scheme, netloc = parse.splittype(url)
+    host, document = parse.splithost(netloc)
     port = default_ports.get(scheme, 0)
     if host:
         host = host.lower()