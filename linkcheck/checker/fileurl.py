PetrDlouhy/linkchecker-1

Python3: fix unicode in fileurl

linkcheck/checker/fileurl.py
@@ -31,6 +31,7 @@ try:
 except ImportError:
     # Python 3
     from urllib.request import urlopen
+from builtins import str as text
 from datetime import datetime
 
 from . import urlbase, get_index_html
@@ -135,7 +136,10 @@ class FileUrl (urlbase.UrlBase):
             base_url = re.sub("^file://(/?)([a-zA-Z]):", r"file:///\2|", base_url)
             # transform file://path into file:///path
             base_url = re.sub("^file://([^/])", r"file:///\1", base_url)
-        self.base_url = unicode(base_url)
+        try:
+            self.base_url = unicode(base_url)
+        except NameError:
+            self.base_url = base_url
 
     def build_url (self):
         """
@@ -211,7 +215,7 @@ class FileUrl (urlbase.UrlBase):
         with links to the files."""
         if self.is_directory():
             data = get_index_html(get_files(self.get_os_filename()))
-            if isinstance(data, unicode):
+            if isinstance(data, text):
                 data = data.encode("iso8859-1", "ignore")
         else:
             data = super(FileUrl, self).read_content()

linkchecker/linkchecker

linkcheck/checker/fileurl.py
@@ -34,6 +34,7 @@ try:
 except ImportError:
     # Python 3
     from urllib.request import urlopen
+from builtins import str as str_text
 from datetime import datetime
 
 from . import urlbase, get_index_html
@@ -138,7 +139,10 @@ class FileUrl (urlbase.UrlBase):
             base_url = re.sub("^file://(/?)([a-zA-Z]):", r"file:///\2|", base_url)
             # transform file://path into file:///path
             base_url = re.sub("^file://([^/])", r"file:///\1", base_url)
-        self.base_url = unicode(base_url)
+        try:
+            self.base_url = unicode(base_url)
+        except NameError:
+            self.base_url = base_url
 
     def build_url (self):
         """
@@ -214,7 +218,7 @@ class FileUrl (urlbase.UrlBase):
         with links to the files."""
         if self.is_directory():
             data = get_index_html(get_files(self.get_os_filename()))
-            if isinstance(data, unicode):
+            if isinstance(data, str_text):
                 data = data.encode("iso8859-1", "ignore")
         else:
             data = super(FileUrl, self).read_content()