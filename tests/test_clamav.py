c71cfcbea4aaa14c22333ecac2ff2cbd6c1fdf69
Chris Mayo <aklhfex@gmail.com>
2020-05-29 19:40:46 +0100

    Tidy TestClamav.testInfected() acceptable_responses

--- tests/test_clamav.py
+++ tests/test_clamav.py
@@ -53,7 +53,9 @@ class TestClamav(unittest.TestCase):
         infected, errors = clamav.scan(data, self.clamav_conf)
         # different versions of clamav report different responses, apparently
         acceptable_responses = (
-            ["stream: ClamAV-Test-File(2d1206194bd704385e37000be6113f73:781) FOUND\n"],
+            [
+              "stream: ClamAV-Test-File(2d1206194bd704385e37000be6113f73:781) FOUND\n"
+            ],
             [
               "stream: Clamav.Test.File-6(aa15bcf478d165efd2065190eb473bcb:544) FOUND\n"
             ],
