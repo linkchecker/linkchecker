1632a1ce266389e679d907e83093abd97c8bc42e
Chris Mayo <aklhfex@gmail.com>
2020-06-05 16:06:01 +0100

    Fix xgettext Non-ASCII error when translating
    xgettext: Non-ASCII character at
    ../linkcheck/plugins/markdowncheck.py:2.
              Please specify the source encoding through --from-code or through a comment
              as specified in https://www.python.org/peps/pep-0263.html.
    
    make: *** [Makefile:25: linkchecker.pot] Error 1

--- linkcheck/plugins/markdowncheck.py
+++ linkcheck/plugins/markdowncheck.py
@@ -1,5 +1,5 @@
 #
-# Copyright © 2014 Vadym Khokhlov
+# Copyright (C) 2014 Vadym Khokhlov
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
